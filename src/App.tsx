6beaa69d2c8e3d576e4e8da90b6fa311900a07a3
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-09-03 11:49:47 +0900

    feat: implement Routing

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import AuthPage from './components/PageAuth';
+import HomePage from './components/PageHome';
+import PrivateRoute from './components/PrivateRoute';
+import * as routes from './routes';
+
+function App() {
+  return (
+    <Routes>
+      <Route path={routes.authPath} element={<AuthPage />} />
+      <Route
+        path={routes.homePath}
+        element={
+          <PrivateRoute
+            element={<HomePage />}
+            generatePath={routes.generateHomePath}
+          />
+        }
+      />
+      <Route path="*" element={<Navigate to={routes.authPath} replace />} />
+    </Routes>
+  );
+}
+
+export default App;
