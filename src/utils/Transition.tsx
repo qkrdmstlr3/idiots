8a2975742ca24842a5a1e7bb3d97aabded0861b4
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-09-03 22:28:53 +0900

    feat: implement Transition util for animation

---
+++ src/utils/Transition.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+type StatusType = 'on' | 'off' | 'oning' | 'offing';
+interface TransitionProps {
+  children: (state: StatusType) => React.ReactNode;
+  isOn?: boolean;
+  timeout?: number;
+}
+
+function Transition({ children, isOn, timeout = 500 }: TransitionProps) {
+  const timerRef = useRef<number>();
+  const [status, setStatus] = useState<StatusType>(isOn ? 'on' : 'off');
+
+  const onTransitionEnd = useCallback(() => {
+    if (status === 'off') return;
+    setStatus('offing');
+    timerRef.current = setTimeout(() => {
+      setStatus('off');
+    }, timeout);
+  }, [setStatus, timeout, status]);
+
+  const onTransitionStart = useCallback(() => {
+    if (status === 'on') return;
+    setStatus('oning');
+    timerRef.current = setTimeout(() => {
+      setStatus('on');
+    }, timeout);
+  }, [setStatus, timeout, status]);
+
+  useEffect(() => {
+    if (!isOn) return onTransitionEnd();
+    onTransitionStart();
+
+    return () => clearTimeout(timerRef.current);
+  }, [isOn, onTransitionEnd, onTransitionStart]);
+
+  if (status === 'off') return <></>;
+  return <>{children(status)}</>;
+}
+
+export default Transition;
