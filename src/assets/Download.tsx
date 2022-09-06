67185089843110892338b7d67068cd723d8d160f
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-09-06 13:00:39 +0900

    refactor: change props of svg

--- src/assets/Download.tsx
+++ src/assets/Download.tsx
@@ -3,11 +3,7 @@ export interface DownloadProps {
 }
 
 const Download: React.FC<DownloadProps> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
     <path
       fill={props.color}
       d="M23 24v-20h-8v2h6v16h-18v-16h6v-2h-8v20h22zm-12-13h-4l5 6 5-6h-4v-11h-2v11z"
--- src/assets/Upload.tsx
+++ src/assets/Upload.tsx
@@ -3,11 +3,7 @@ export interface UploadProps {
 }
 
 const Upload: React.FC<UploadProps> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
     <path
       fill={props.color}
       d="M23 0v20h-8v-2h6v-16h-18v16h6v2h-8v-20h22zm-12 13h-4l5-6 5 6h-4v11h-2v-11z"
